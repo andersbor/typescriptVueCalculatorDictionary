85e35c7a3f05c1e01b24ab833e5b10a5dff95f52
Anders Børjesson <anbo@zealand.dk>
2020-08-26 11:15:55 +0200

    eval included

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,7 +4,10 @@ new Vue({
         number1: 0,
         number2: 0,
         operation: '+',
-        result: 0
+        result: 0,
+        expression: "",
+        result2: 0,
+        stack: ['2', '3', '+']
     },
     methods: {
         calculate: function () {
@@ -21,6 +24,21 @@ new Vue({
                 case "/":
                     this.result = this.number1 / this.number2;
                     break;
+            }
+        },
+        calculate2: function () {
+            //console.log(this.expression)
+            this.result2 = eval(this.expression)
+            //console.log(this.result2)
+            while (this.stack.length > 1) {
+                let element1 = this.stack.pop()
+                if (element1 == "+") {
+                    let operand1 = Number(this.stack.pop())
+                    let operand2 = Number(this.stack.pop())
+                    let result = operand1 + operand2
+                    console.log(result)
+                    this.stack.push(result)
+                }
             }
         }
     }
